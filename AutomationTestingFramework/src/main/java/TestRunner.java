95c56c65aaddc85080fa605ce7afaeaecaf28ea7
Felicia Starinschi <starinschifeliciav@gmail.com>
2023-03-21 17:11:08 +0100

    added instance creation of  the firefox webdriver

--- AutomationTestingFramework/src/main/java/TestRunner.java
+++ AutomationTestingFramework/src/main/java/TestRunner.java
@@ -2,8 +2,12 @@ import managers.WebDriversManager;
 
 public class TestRunner {
     public static void main(String[] args){
-        WebDriversManager webDriversManager = new WebDriversManager(1);
-        webDriversManager.getDriver().get("https://www.google.com/");
-        webDriversManager.closeDriver();
+        WebDriversManager webDriverChrome = new WebDriversManager(1);
+        webDriverChrome.getDriver().get("https://www.google.com/");
+        webDriverChrome.closeDriver();
+
+        WebDriversManager webDriverFirefox= new WebDriversManager(2);
+        webDriverFirefox.getDriver().get("https://www.google.com/");
+        webDriverFirefox.closeDriver();
     }
 }
