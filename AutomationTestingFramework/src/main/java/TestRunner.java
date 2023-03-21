2668c66ff1f9bd095cab3ceed3fc4e4894cefb3e
Felicia Starinschi <starinschifeliciav@gmail.com>
2023-03-21 16:59:05 +0100

    implemented logics of the wev driver manager

--- AutomationTestingFramework/src/main/java/TestRunner.java
+++ AutomationTestingFramework/src/main/java/TestRunner.java
@@ -1,2 +1,9 @@
+import managers.WebDriversManager;
+
 public class TestRunner {
+    public static void main(String[] args){
+        WebDriversManager webDriversManager = new WebDriversManager(1);
+        webDriversManager.getDriver().get("https://www.google.com/");
+        webDriversManager.closeDriver();
+    }
 }
